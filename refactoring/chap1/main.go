36ab0d3f490aa84c9e3a7f76ec04e5366a47679d
Shinya Yaginuma <yaginuuun@gmail.com>
2022-05-21 21:36:25 +0900

    fix main.go

--- refactoring/chap1/main.go
+++ refactoring/chap1/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"math"
 
 	"github.com/play-with-golang/refactoring/chap1/model"
+	"github.com/play-with-golang/refactoring/chap1/statement"
 )
 
 func main() {
@@ -14,98 +14,16 @@ func main() {
 			{PlayID: "hamlet", Audience: 55},
 		},
 	}
-	data.TotalAmount = totalAmount(data.Performances)
-	data.TotalVolumeCredits = totalVolumeCredits(data.Performances)
-	return renderPlainText(data)
+	data := statement.NewStatement(invoice)
+	fmt.Println(renderPlainText(data))
 }
 
-func (perf *Performance) enrich() {
-	perf.Play = playFor(*perf)
-	amount, err := amountFor(*perf)
-	if err != nil {
-		fmt.Println(err)
-	}
-	perf.Amount = amount
-	perf.VolumeCredits = volumeCreditsFor(*perf)
-}
-
-func renderPlainText(data StatementData) (string, error) {
+func renderPlainText(data statement.StatementData) string {
 	result := fmt.Sprintf("Statement for %s\n", data.Customer)
 	for _, perf := range data.Performances {
 		result += fmt.Sprintf("\t%s: %v (%v seat)\n", perf.PlayID, perf.Amount, perf.Audience)
 	}
 	result += fmt.Sprintf("Amount owed is %v\n", data.TotalAmount)
 	result += fmt.Sprintf("You earned %v credits\n", data.TotalVolumeCredits)
-	return result, nil
-}
-
-func totalVolumeCredits(p []*Performance) int {
-	result := 0
-	for _, perf := range p {
-		result += perf.VolumeCredits
-	}
 	return result
-}
-
-func totalAmount(p []*Performance) int {
-	result := 0
-	for _, perf := range p {
-		result += perf.Amount
-	}
-	return result
-}
-
-func amountFor(perf Performance) (int, error) {
-	result := 0
-
-	switch perf.Play.PlayType {
-	case "tragedy":
-		result = 40000
-		if perf.Audience > 30 {
-			result += 1000 * (perf.Audience - 30)
-		}
-	case "comedy":
-		result = 30000
-		if perf.Audience > 20 {
-			result += 10000 + 500*(perf.Audience-20)
-		}
-		result += 300 * perf.Audience
-	default:
-		return 0, fmt.Errorf("unknown type: %s", perf.Play.PlayType)
-	}
-
-	return result, nil
-}
-
-func volumeCreditsFor(perf Performance) int {
-	result := 0
-	result += int(math.Max(float64(perf.Audience-30), 0.0))
-	if perf.Play.PlayType == "comedy" {
-		result += int(math.Floor(float64(perf.Audience) / 5))
-	}
-	return result
-}
-
-func playFor(perf Performance) Play {
-	plays := map[string]Play{
-		"hamlet":  {"Hamlet", "tragedy"},
-		"as-like": {"As You Like It", "comedy"},
-		"othello": {"Othello", "tragedy"},
-	}
-	return plays[perf.PlayID]
-}
-
-func main() {
-	invoice := Invoice{
-		"Yagi",
-		[]*Performance{
-			{PlayID: "hamlet", Audience: 55},
-		},
-	}
-
-	result, err := statement(invoice)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(result)
 }
